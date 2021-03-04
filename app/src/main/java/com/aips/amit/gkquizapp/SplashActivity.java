92852dd2beb6d410a3477edc23b9a642cfa138e7
Amit Kumar <amitkv1212@gmail.github.com>
2021-03-04 13:42:03 +0530

    Added Files till Video 1

---
+++ app/src/main/java/com/aips/amit/gkquizapp/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.aips.amit.gkquizapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000);
+    }
+}
